3ad509dd42aad2ea067a2dcb63913f3ee5fa3940
makodevsupport <dev@makoitlab.com>
2021-02-25 16:00:52 +0530

    packages included

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const GRAPHQL_ENDPOINT = "https://hasura-realtime-group-chat.herokuapp.com/v1alpha1/graphql";
+
+// Make WebSocketLink with appropriate url
+const mkWsLink = (uri) => {
+  const splitUri = uri.split('//');
+  const subClient = new SubscriptionClient(
+    'wss://' + splitUri[1],
+    { reconnect: true }
+  );
+  return new WebSocketLink(subClient);
+}
+
+// Makle HttpLink
+const httpLink = new HttpLink({ uri: GRAPHQL_ENDPOINT });
+const wsLink = mkWsLink(GRAPHQL_ENDPOINT);
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Instantiate client
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache({
+    addTypename: false
+  })
+})
+
+ReactDOM.render(
+  (<ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>),
+  document.getElementById('root')
+);
